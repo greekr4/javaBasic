f95aabca459f2d7b88583ab956dd0ae05bd4e891
greekr4 <greekr4@naver.com>
2022-04-06 17:15:05 +0900

    d

---
+++ ch7/src/sec4/Child2.java
@@ -0,0 +1,12 @@
+package sec4;
+//구현 클래스 : 추상 클래스의 자식 클래스
+public class Child2 extends Abstract1{
+
+	@Override
+	void method1() {
+		System.out.println("자식2의 메서드 1");
+	}
+
+
+	
+}
---
+++ ch7/src/sec4/ex1.java
@@ -0,0 +1,13 @@
+package sec4;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		SmartPhone a = new SmartPhone("zz");
+		a.turnOn();
+		a.internetSearch();
+		a.turnOff();
+
+	}
+
+}
